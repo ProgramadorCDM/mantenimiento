5ddaf5bb2426de82f2686d882fde340244c3f91e
Jorge Mina <minajorge@hotmail.com>
2020-12-14 09:47:43 -0500

    Security Repositories Created

---
+++ src/main/java/com/cdmservicios/mantenimiento/security/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.cdmservicios.mantenimiento.security.repositories;
+
+import com.cdmservicios.mantenimiento.security.models.ERole;
+import com.cdmservicios.mantenimiento.security.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+    Optional<Role> findAllByName(ERole name);
+}
